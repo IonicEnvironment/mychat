618f86a8b9227b6dc3bcc2e8a2c15ebb3be94170
Arya Kumar <aryajeswaltelerik@gmail.com>
2015-04-22 13:07:40 +0000

    Changes made by Arya Kumar (aryajeswaltelerik@gmail.com).

--- Ionic_Chat/project/script/app.js
+++ Ionic_Chat/project/script/app.js
@@ -1,5 +1,5 @@
-angular.module('mobreg', ['ionic'])
-.config(function($stateProvider,$urlRouterProvider){
+var app = angular.module('mobreg', ['ionic'])
+app.config(function($stateProvider,$urlRouterProvider){
     $urlRouterProvider.otherwise('/')
     
     $stateProvider
@@ -11,4 +11,55 @@ angular.module('mobreg', ['ionic'])
         url:'/',
         templateUrl:'signup.html'
     })
-})
+});
+
+app.run(function($ionicPlatform){
+    $ionicPlatform.ready(function(){
+        console.log(navigator.contacts);
+        adata();
+        
+        function adata()
+        {
+            var options = new ContactFindOptions();
+                options.filter="";
+                options.multiple=true; 
+                var fields = ["*"];
+                navigator.contacts.find(fields, onSuccess, onError, options);
+        }
+        
+        function onSuccess(contacts) {
+            alert(JSON.stringify(contacts[7]));
+                for (var i = 0; i < contacts.length; i++) {
+                    
+                    alert(JSON.stringify(contacts[i].phoneNumber[0].value));
+                }
+            }
+        
+        function onError()
+{
+alert("Some Error Occured");
+}
+    });
+});
+app.controller('mobileregister',['$scope','$state',function($scope,$state){
+    $scope.callme = function()
+    {
+        if(angular.isUndefined(this.fname) || this.fname === null)
+        {
+            navigator.notification.alert("Please enter Firstname",function(){},"Notification","OK");
+        }
+        else if(angular.isUndefined(this.lname) || this.lname === null)
+        {
+            navigator.notification.alert("Please enter Lastname",function(){},"Notification","OK");
+        }
+        else if(angular.isUndefined(this.mobnum) || this.mobnum === null)
+        {
+            navigator.notification.alert("Please enter Mobile Number",function(){},"Notification","OK");
+        }
+        else
+        {
+            alert("name : "+this.fname+" "+this.lname+"\nmobile number : "+this.mobnum);
+            $state.go("signup");
+        }
+    }
+}]);
